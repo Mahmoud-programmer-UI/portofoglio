018b4e6c4d358edfb6f85b9cefd6cf3961790b15
Mahmoud Mohamed <143353397+Mahmoud-programmer-UI@users.noreply.github.com>
2025-06-29 13:53:54 +0000

    update all changes

--- src/About.js
+++ src/About.js
@@ -1,6 +1,4 @@
-import Footer from './footer'
-import {   Link} from "react-router-dom";
-import './assets//stylesheets//about.css'
+
 function About(){
     return(
         <><div className='about-page-section'>
